0f802c3cc51623b8c2fabf51094b0423caf25b0e
liz-miller <liz.miller11@gmail.com>
2020-06-18 13:39:58 -0400

    update comments on my-account.php

--- my-account.php
+++ my-account.php
@@ -1,8 +1,8 @@
 /*
  * Add affiliate panel to My Account
  */
-add_filter ( 'woocommerce_account_menu_items', 'lr_affiliate_dashboard_link', 40 );
-function lr_affiliate_dashboard_link( $menu_links ){
+add_filter ( 'woocommerce_account_menu_items', 'new_affiliate_dashboard_link', 40 );
+function new_affiliate_dashboard_link( $menu_links ){
 
 	$menu_links = array_slice( $menu_links, 0, 7, true )
 	+ array( 'affiliate-dashboard' => 'Affiliates' )
@@ -11,13 +11,16 @@ function lr_affiliate_dashboard_link( $menu_links ){
 	return $menu_links;
 }
 
-add_action( 'init', 'lr_add_endpoint' );
-function lr_add_endpoint() {
+add_action( 'init', 'new_add_endpoint' );
+function new_add_endpoint() {
 	add_rewrite_endpoint( 'affiliate-dashboard', EP_PAGES );
 }
 
-add_action( 'woocommerce_account_affiliate-dashboard_endpoint', 'lr_my_account_endpoint_content' );
-function lr_my_account_endpoint_content() {
+/*
+ * This is where you can change the content of the new Affiliates Tab
+ */
+add_action( 'woocommerce_account_affiliate-dashboard_endpoint', 'new_my_account_endpoint_content' );
+function new_my_account_endpoint_content() {
   echo '<h3>Join our Affiliate Program!</h3><p>Welcome to the Affiliate Dashboard!</p>';
   echo do_shortcode( ' [AffiliatesLogin] ' );
   echo '<br>';
